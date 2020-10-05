573a94f1bca07c2415b71299e9fa68097890031f
marcis.gulbis <mg3001@inbox.lv>
2020-10-05 12:38:24 +0300

    Refactored code

--- game_of_life/game_of_life/Program.cs
+++ game_of_life/game_of_life/Program.cs
@@ -6,107 +6,85 @@ namespace game_of_life
 {
     class Program
     {
-        static int Rows;
-        static int Columns;
-        static bool shouldRunSimulation = true;
-
         static void Main(string[] args)
         {
-            // Create grid of simulation
-            Cell[,] grid = new Cell[1, 1];
-            Console.WriteLine("Do you wish to load a saved file? (Y-yes)");
+            int rows = 3;
+            int cols = 3;
+            Simulation simulation;
+            bool shouldRunSimulation = true;
+
+            Console.WriteLine("Welcome to the Game of Life!\n" +
+                "Would you like to load from a file? (Y-yes, other-no)");
             string load = Console.ReadLine();
+
             if (load == "Y")
             {
-                Console.WriteLine("Please enter the name of the saved file.");
+                Console.WriteLine("Please enter the name of the file! (without .txt)");
                 string fileName = Console.ReadLine();
-                string[] lines = System.IO.File.ReadAllLines(@"./" + fileName + ".txt");
-                Rows = Int32.Parse(lines[0].Substring(0, lines[0].IndexOf(',')));
-                Columns = Int32.Parse(lines[0].Substring(lines[0].IndexOf(',') + 1));
-                grid = new Cell[Rows, Columns];
 
-                for (int x = 0; x < Rows; x++)
-                {
-                    for (int y = 0; y < Columns; y++)
-                    {
-                        try
-                        {
-                            grid[x, y] = new Cell(false);
-                            if (lines[x + 1][y] == '+')
-                            {
-                                grid[x, y].isAlive = true;
-                            }
-                        }
-                        catch { }
-                    }
-                }
+                string[] lines = System.IO.File.ReadAllLines(@"./" + fileName + ".txt");
+
+                simulation = new Simulation(lines);
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine("Enter ammount of rows for simulation:");
-                try
+                string cont = "";
+                do
                 {
-                    Rows = Int32.Parse(Console.ReadLine());
-                    if (Rows < 2)
-                        throw new Exception();
-                }
-                catch
-                {
-                    Console.WriteLine("Incorrect input, defaulting to 30 rows.");
-                    Rows = 30;
-                }
-
-                Console.WriteLine("Enter amount of columns for simulation:");
-                try
-                {
-                    Columns = Int32.Parse(Console.ReadLine());
-                    if (Columns < 2)
-                        throw new Exception();
-                }
-                catch
-                {
-                    Console.WriteLine("Incorrect input, defaulting to 60 columns.");
-                    Columns = 60;
-                }
-
-                //Initialise randomized grid
-                grid = new Cell[Rows, Columns];
-                for (int x = 0; x < Rows; x++)
-                {
-                    for (int y = 0; y < Columns; y++)
+                    Console.WriteLine("\nPlease enter the amount of rows for simulation!");
+                    try
                     {
-                        grid[x, y] = new Cell(true);
+                        rows = Int32.Parse(Console.ReadLine());
+                        if (rows < 3)
+                            throw new Exception();
                     }
-                }
+                    catch
+                    {
+                        Console.WriteLine("Error in input, defaulting to 30 rows.");
+                        rows = 30;
+                    }
 
-                Console.WriteLine("Press any key to continue");
-                Console.ReadKey();
+                    Console.WriteLine("Please enter the amount of columns for simulation!");
+                    try
+                    {
+                        cols = Int32.Parse(Console.ReadLine());
+                        if (cols < 3)
+                            throw new Exception();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error in input, defaulting to 60 columns.");
+                        cols = 60;
+                    }
+
+                    Console.WriteLine("\nAre these inputs correct?\n" +
+                        "Rows: " + rows + "\nColumns: " + cols +
+                        "\nEnter \"Y\" to continue, or enter anything else to go back and change inputs!");
+                    cont = Console.ReadLine();
+                    Console.Clear();
+                } while (cont != "Y");
+
+                rows += 2;
+                cols += 2;
+                simulation = new Simulation(rows, cols);
             }
-            
-            // Display grid
-            Console.Clear();
+
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
             Console.CursorVisible = false;
-            if (Rows < 10 || Rows > 50)
-                Console.WindowHeight = 35;
-            else
-                Console.WindowHeight = Rows + 2;
-            if (Columns < 90 || Columns > 150)
-                Console.WindowWidth = 90;
-            else
-                Console.WindowWidth = Columns;
-            int currentIteration = 1;
-            PrintGrid(grid, currentIteration);
+
+            Console.Clear();
+            Console.Write(simulation.ToStringBuilder());
+
             while (shouldRunSimulation)
             {
-                //If no key is pressed, continues the game
+                Thread.Sleep(1000);
                 if (!Console.KeyAvailable)
                 {
-                    grid = NextIteration(grid);
-                    currentIteration++;
-                    PrintGrid(grid, currentIteration);
+                    simulation.NextIteration();
+                    Console.Clear();
+                    Console.Write(simulation.ToStringBuilder());
                 }
-                //If a key is pressed, checks if it is SPACE or ESC for PAUSE and STOP respectively
                 else
                 {
                     ConsoleKeyInfo key = Console.ReadKey(false);
@@ -116,11 +94,11 @@ namespace game_of_life
                         {
                             Console.CursorTop = 1;
                             Console.CursorLeft = 1;
-                            Console.Write("CURRENTLY PAUSED");
+                            Console.Write("SIMULATION PAUSED");
                             key = Console.ReadKey(false);
                             if (key.Key == ConsoleKey.Escape)
                             {
-                                Console.CursorTop = Rows + 2;
+                                Console.CursorTop = rows + 2;
                                 Console.CursorLeft = 0;
                                 break;
                             }
@@ -133,157 +111,16 @@ namespace game_of_life
                 }
             }
 
+            Console.CursorVisible = true;
             Console.WriteLine("WWould you like to save information to a file? (Y-yes)");
             string save = Console.ReadLine();
             if (save == "Y")
             {
-                //Create string array to save info
-                string[] gridInfo = new string[Rows + 1];
-                gridInfo[0] = Rows + "," + Columns;
+                string[] gridInfo = simulation.ToSaveable();
 
-                //Populates string array with info about grid
-                for (int x = 0; x < Rows; x++)
-                {
-                    for (int y = 0; y < Columns; y++)
-                    {
-                        if (grid[x, y].isAlive)
-                        {
-                            gridInfo[x] += '+';
-                        }
-                        else
-                        {
-                            gridInfo[x] += ' ';
-                        }
-                    }
-                }
-
-                //Asks for file name and saves it
-                Console.WriteLine("Please enter the name for this file:");
+                Console.WriteLine("Please enter a name for this file!");
                 string fileName = Console.ReadLine();
                 System.IO.File.WriteAllLines(@"./" + fileName + ".txt", gridInfo);
-            }
-        }
-
-        private static Cell[,] NextIteration(Cell[,] currentGrid)
-        {
-            Cell[,] nextGrid = new Cell[Rows, Columns];
-            for (int x = 0; x < Rows; x++)
-            {
-                for (int y = 0; y < Columns; y++)
-                {
-                    nextGrid[x, y] = new Cell(false);
-                }
-            }
-
-            //Loop through each cell in grid
-            for (int x = 1; x < Rows - 1; x++)
-            {
-                for (int y = 1; y < Columns - 1; y++)
-                {
-                    //Find count of neigbhors for current cell
-                    int neighborsCount = -1;
-                    for (int i = -1; i < 2; i++)
-                    {
-                        for (int j = -1; j < 2; j++)
-                        {
-                            if (currentGrid[x+i, y+j].isAlive)
-                            {
-                                neighborsCount++;
-                            }
-                        }
-                    }
-
-                    //Checks if cell is alive
-                    if (currentGrid[x, y].isAlive)
-                    {
-                        //Checks alive cell if it is underpopulated or overpopulated, kills it if it is
-                        if (neighborsCount < 2 || neighborsCount > 3)
-                        {
-                            nextGrid[x, y].isAlive = false;
-                        }
-                        else
-                        {
-                            nextGrid[x, y].isAlive = true;
-                        }
-                    }
-                    else
-                    {
-                        //Checks dead cell if it can be reproduced, does so if it can
-                        if (neighborsCount > 2)
-                        {
-                            nextGrid[x, y].isAlive = true;
-                        }
-                        else
-                        {
-                            nextGrid[x, y].isAlive = false;
-                        }
-                    }
-                }
-            }
-
-            return nextGrid;
-        }
-
-        private static void PrintGrid(Cell[,] grid, int iterationCount)
-        {
-            var printableGrid = new StringBuilder();
-
-            //Count live cells
-            int aliveCellCount = 0;
-            foreach(Cell cell in grid)
-            {
-                if (cell.isAlive)
-                {
-                    aliveCellCount++;
-                }
-            }
-
-            //Write iteration count and count of live cells
-            printableGrid.Append("Current iteration: " + iterationCount +
-                "; Count of live cells: " + aliveCellCount +
-                "; press SPACE to pause, press ESC to stop\n");
-
-            // Generate printable grid from given grid
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int col = 0; col < Columns; col++)
-                {
-                    if (grid[row, col].isAlive)
-                    {
-                        printableGrid.Append('+');
-                    }
-                    else
-                    {
-                        printableGrid.Append(' ');
-                    }
-                }
-                printableGrid.Append('\n');
-            }
-            Console.Clear();
-            //Console.SetCursorPosition(0, 0);
-            Console.Write(printableGrid);
-            Thread.Sleep(1000);
-        }
-    }
-
-    class Cell
-    {
-        public bool isAlive { get; set; }
-
-        public Cell(bool randomize)
-        {
-            isAlive = false;
-            if (randomize)
-            {
-                Random rand = new Random();
-                if (rand.Next(2) > 0)
-                {
-                    isAlive = true;
-                }
-                else
-                {
-                    isAlive = false;
-                }
             }
         }
     }
---
+++ game_of_life/game_of_life/Simulation.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace game_of_life
+{
+    class Simulation
+    {
+        public int IterationCount { get; private set; }
+        public int CellCount { get; private set; }
+        private int Rows { get; set; }
+        private int Columns { get; set; }
+        private bool[,] Grid { get; set; }
+
+        public Simulation(int rows, int cols)
+        {
+            IterationCount = 0;
+            CellCount = 0;
+            Rows = rows;
+            Columns = cols;
+            GenerateRandom();
+        }
+
+        public Simulation(string[] fileGrid)
+        {
+            IterationCount = Int32.Parse(fileGrid[0].Substring(0, fileGrid[0].IndexOf(',')));
+            CellCount = Int32.Parse(fileGrid[0].Substring(fileGrid[0].IndexOf(',') + 1));
+            Rows = Int32.Parse(fileGrid[1].Substring(0, fileGrid[1].IndexOf(',')));
+            Columns = Int32.Parse(fileGrid[1].Substring(fileGrid[1].IndexOf(',') + 1));
+
+            bool[,] grid = new bool[Rows, Columns];
+            for (int x = 0; x < Rows; x++)
+            {
+                for (int y = 0; y < Columns; y++)
+                {
+                    try
+                    {
+                        if (fileGrid[x + 2][y] == '+')
+                        {
+                            grid[x, y] = true;
+                        }
+                        else
+                        {
+                            grid[x, y] = false;
+                        }
+                    }
+                    catch { }
+                }
+            }
+            Grid = grid;
+        }
+
+        private void GenerateRandom()
+        {
+            Grid = new bool[Rows, Columns];
+            for (int x = 1; x < Rows - 1; x++)
+            {
+                for (int y = 1; y < Columns - 1; y++)
+                {
+                    Random rand = new Random();
+                    if (rand.Next(2) > 0)
+                    {
+                        Grid[x, y] = true;
+                        CellCount++;
+                    }
+                    else
+                    {
+                        Grid[x, y] = false;
+                    }
+                }
+            }
+        }
+
+        public StringBuilder ToStringBuilder()
+        {
+            StringBuilder print = new StringBuilder();
+
+            print.Append("Current iteration: " + IterationCount +
+                "; Count of live cells: " + CellCount +
+                "; press SPACE to pause, press ESC to stop\n");
+
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    if (Grid[row, col])
+                    {
+                        print.Append('+');
+                    }
+                    else
+                    {
+                        print.Append(' ');
+                    }
+                }
+                print.Append('\n');
+            }
+
+            return print;
+        }
+
+        public void NextIteration()
+        {
+            bool[,] nextGrid = new bool[Rows, Columns];
+            IterationCount++;
+
+            for (int x = 1; x < Rows - 1; x++)
+            {
+                for (int y = 1; y < Columns - 1; y++)
+                {
+                    //Find count of neigbhors for current cell
+                    int neighborsCount = -1;
+                    for (int i = -1; i < 2; i++)
+                    {
+                        for (int j = -1; j < 2; j++)
+                        {
+                            if (Grid[x + i, y + j])
+                            {
+                                neighborsCount++;
+                            }
+                        }
+                    }
+
+                    //Checks if cell is alive
+                    if (Grid[x, y])
+                    {
+                        //Checks alive cell if it is underpopulated or overpopulated, kills it if it is
+                        if (neighborsCount < 2 || neighborsCount > 3)
+                        {
+                            nextGrid[x, y] = false;
+                            CellCount--;
+                        }
+                        else
+                        {
+                            nextGrid[x, y] = true;
+                        }
+                    }
+                    else
+                    {
+                        //Checks dead cell if it can be reproduced, does so if it can
+                        if (neighborsCount > 2)
+                        {
+                            nextGrid[x, y] = true;
+                            CellCount++;
+                        }
+                        else
+                        {
+                            nextGrid[x, y] = false;
+                        }
+                    }
+                }
+            }
+
+            Grid = nextGrid;
+        }
+
+        public string[] ToSaveable()
+        {
+            string[] lines = new string[Rows + 2];
+
+            lines[0] = IterationCount + "," + CellCount;
+            lines[1] = Rows + "," + Columns;
+
+            for (int x = 0; x < Rows; x++)
+            {
+                for (int y = 0; y < Columns; y++)
+                {
+                    if (Grid[x, y])
+                    {
+                        lines[x + 2] += '+';
+                    }
+                    else
+                    {
+                        lines[x + 2] += ' ';
+                    }
+                }
+            }
+
+            return lines;
+        }
+    }
+}
