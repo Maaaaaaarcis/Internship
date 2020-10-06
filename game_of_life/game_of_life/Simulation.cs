53f1e9f2e30331d7dff293747023096805f66e36
marcis.gulbis <mg3001@inbox.lv>
2020-10-06 10:53:26 +0300

    Finished file saving to JSON

--- game_of_life/game_of_life/Simulation.cs
+++ game_of_life/game_of_life/Simulation.cs
@@ -34,7 +34,7 @@ namespace GameOfLife
         /// <summary>
         /// Gets bool array of simulation's grid
         /// </summary>
-        public bool[,] Grid { get; set; }
+        public bool[][] Grid { get; set; }
 
         /// <summary>
         /// Class that contains logic for the simulation and its proccesses
@@ -58,7 +58,11 @@ namespace GameOfLife
             Columns = cols;
 
             //Initialises grid of the simulation
-            Grid = new bool[Rows, Columns];
+            Grid = new bool[Rows][];
+            for (int i = 0; i < Rows; i++)
+            {
+                Grid[i] = new bool[Columns];
+            }
 
             //For each cell in the grid that is not on the border this will assign
             //a randomised value of either true or false
@@ -69,12 +73,12 @@ namespace GameOfLife
                     Random rand = new Random();
                     if (rand.Next(2) > 0)
                     {
-                        Grid[x, y] = true;
+                        Grid[x][y] = true;
                         CellCount++;
                     }
                     else
                     {
-                        Grid[x, y] = false;
+                        Grid[x][y] = false;
                     }
                 }
             }
@@ -113,7 +117,7 @@ namespace GameOfLife
             {
                 for (int col = 0; col < Columns; col++)
                 {
-                    if (Grid[row, col])
+                    if (Grid[row][col])
                     {
                         print.Append('+');
                     }
@@ -133,8 +137,11 @@ namespace GameOfLife
         /// </summary>
         public void NextIteration()
         {
-            bool[,] nextGrid = new bool[Rows, Columns];
-            IterationCount++;
+            bool[][] nextGrid = new bool[Rows][];
+            for (int i = 0; i<Rows; i++)
+            {
+                nextGrid[i] = new bool[Columns];
+            }
 
             for (int x = 1; x < Rows - 1; x++)
             {
@@ -146,7 +153,7 @@ namespace GameOfLife
                     {
                         for (int j = -1; j < 2; j++)
                         {
-                            if (Grid[x + i, y + j])
+                            if (Grid[x + i][y + j])
                             {
                                 neighborsCount++;
                             }
@@ -154,17 +161,17 @@ namespace GameOfLife
                     }
 
                     //Checks if cell is alive
-                    if (Grid[x, y])
+                    if (Grid[x][y])
                     {
                         //Checks alive cell if it is underpopulated or overpopulated, kills it if it is
                         if (neighborsCount < 2 || neighborsCount > 3)
                         {
-                            nextGrid[x, y] = false;
+                            nextGrid[x][y] = false;
                             CellCount--;
                         }
                         else
                         {
-                            nextGrid[x, y] = true;
+                            nextGrid[x][y] = true;
                         }
                     }
                     else
@@ -172,18 +179,19 @@ namespace GameOfLife
                         //Checks dead cell if it can be reproduced, does so if it can
                         if (neighborsCount > 2)
                         {
-                            nextGrid[x, y] = true;
+                            nextGrid[x][y] = true;
                             CellCount++;
                         }
                         else
                         {
-                            nextGrid[x, y] = false;
+                            nextGrid[x][y] = false;
                         }
                     }
                 }
             }
 
             Grid = nextGrid;
+            IterationCount++;
         }
 
         /// <summary>
